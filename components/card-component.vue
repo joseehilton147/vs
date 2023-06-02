5fd5330f2fbd11510e20844de34e54949ae82263
Jose Hilton de Luna Júnior <jose.junior@cliqx.com.br>
2023-06-02 17:55:09 -0300

    fix: border on card dark mode

--- components/card-component.vue
+++ components/card-component.vue
@@ -4,13 +4,13 @@
 
 <template>
 	<div class="rounded-md bg-white shadow-sm dark:bg-neutral-900">
-		<header v-if="slots?.header" class="p-x-4 p-t-4 border-b">
+		<header v-if="slots?.header" class="p-x-4 p-t-4 dark:border-color-neutral-800 border-b">
 			<slot name="header" />
 		</header>
 		<main class="p-4">
 			<slot />
 		</main>
-		<footer v-if="slots?.footer" class="p-x-4 p-b-4 border-t">
+		<footer v-if="slots?.footer" class="p-x-4 p-b-4 dark:border-color-neutral-800 border-t">
 			<slot name="footer" />
 		</footer>
 	</div>
